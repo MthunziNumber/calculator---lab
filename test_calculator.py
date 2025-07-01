d0b1fb20a576e0ca7944d5d0c96fe5b1ce7546a5
Thulani_Mthunzi_Maseko <thulanimthunzimaseko@yahoo.co.za>
2025-07-02 00:15:52 +0200

    Update test_calculator.py
    rectified the spelling error and added more tests

--- test_calculator.py
+++ test_calculator.py
@@ -4,12 +4,20 @@ import unittest
 class TestCalculator(unittest.TestCase):
   def test_add(self):
     self.assertEqual(add(2,3), 5)
+    self.asserEqual(add(-3,-2), -5)
+    self.assertEqual(add(-5,2), -3)
 
   def test_subtract(self):
     self.assertEqual(subtract(10,8), 2)
+    self.assertEqual(subtract(-10, -2), -8)
+    self.assertEqual(subtract(-8, 2), -10)
 
   def test_multiply(self):
-    self.asertEqual(multiply(3,2), 6)
-
+    self.assertEqual(multiply(3,2), 6)
+    self.assertEquals(multiply(-3, -4), 12)
+    self.assertEquals(multiply(-3, 4), -12)
   def test_divide(self):
     self.assertEqual(divide(10,2), 5)
+    self.assertEqual(divide(10, -5), -2)
+    self.assertEqual(divide(-9, -3), 3)   
+    self.assertEqual(divide(10,0), "division by zero is undefined")
